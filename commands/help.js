e3fedd64a7282faa4dc857cbd4e8896871556b28
Ituriel <80064281+nulledituriel@users.noreply.github.com>
2021-04-03 18:31:00 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,8 @@ module.exports = {
         const embed = new MessageEmbed()
         .setTitle('Commands')
         .setThumbnail(client.user.displayAvatarURL())
-        .addField('Tools', `\r\n${config.prefix}ipinfo [ip address]\r\n${config.prefix}nmap [ip address / url]\r\n${config.prefix}dnslookup [url]\r\n${config.prefix}subnetlookup [ip address]\r\n${config.prefix}reversedns [ip address]\r\n${config.prefix}bypass [linkvertise shit]\r\n`)
+        .addField('Network Tools', `\r\n${config.prefix}ipinfo [ip address]\r\n${config.prefix}nmap [ip address / url]\r\n${config.prefix}dnslookup [url]\r\n${config.prefix}subnetlookup [ip address]\r\n${config.prefix}reversedns [ip address]`)
+        .addField('Other Tools', `\r\n${config.prefix}hextostring [hexadecimal input]\r\n${config.prefix}stringtohex [string]\r\n${config.prefix}dectohex [number]\r\n`)
         .setColor("#39d19c")
         .setFooter('itUtils', client.user.displayAvatarURL());
         
